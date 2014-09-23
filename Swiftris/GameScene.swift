d22a4b463aac43569a61be28818ce6a86ed7896f
Paulo Silva <paulosilva86@gmail.com>
2014-09-23 14:59:28 +0100

    Step 6 - Block Party.

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -8,38 +8,50 @@
 
 import SpriteKit
 
+let TickLengthLevelOne = NSTimeInterval(600)
+
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+  var tick:(() -> ())?
+  var tickLengthMillis = TickLengthLevelOne
+  var lastTick:NSDate?
+    
+  required init(coder aDecoder: NSCoder!) {
+    fatalError("NSCoder not supported")
+  }
+    
+  override init(size: CGSize) {
+    super.init(size: size)
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+    let background = SKSpriteNode(imageNamed: "background")
+    background.position = CGPoint(x: 0, y: 0)
+    background.anchorPoint = CGPoint(x: 0, y: 1.0)
+    addChild(background)
+  }
+       
+  override func update(currentTime: CFTimeInterval) {
+    /* Called before each frame is rendered */
+        
+    if (lastTick == nil) {
+      return
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        
+    var timePassed = lastTick!.timeIntervalSinceNow * -1000.0
+        
+    if (timePassed > tickLengthMillis) {
+      lastTick = NSDate.date()
+      tick?()
     }
+        
+  }
+  
+  func startTicking() {
+    lastTick = NSDate.date()
+  }
+  
+  func stopTicking() {
+    lastTick = nil
+  }
 }
