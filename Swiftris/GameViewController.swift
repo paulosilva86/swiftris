dac18f6e334fd778f486323251de4edc3681f9ed
Paulo Silva <paulosilva86@gmail.com>
2014-09-24 18:31:19 +0100

    Move blocks left, right and down.

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController, SwiftrisDelegate {
+class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
   
   var scene: GameScene!
   var swiftris:Swiftris!
+  
+  var panPointReference:CGPoint?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -37,6 +39,49 @@ class GameViewController: UIViewController, SwiftrisDelegate {
   
   override func prefersStatusBarHidden() -> Bool {
     return true
+  }
+  
+  @IBAction func didPan(sender: UIPanGestureRecognizer) {
+    let currentPoint = sender.translationInView(self.view)
+    
+    if let originalPoint = panPointReference {
+      if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+        if sender.velocityInView(self.view).x > CGFloat(0) {
+          swiftris.moveShapeRight()
+          panPointReference = currentPoint
+        } else {
+          swiftris.moveShapeLeft()
+          panPointReference = currentPoint
+        }
+      }
+    } else if sender.state == .Began {
+      panPointReference = currentPoint
+    }
+  }
+  
+  @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+    swiftris.dropShape()
+  }
+  
+  func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+    return true
+  }
+  
+  func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+    if let swipeRec = gestureRecognizer as? UISwipeGestureRecognizer {
+      if let panRec = otherGestureRecognizer as? UIPanGestureRecognizer {
+        return true
+      }
+    } else if let panRec = gestureRecognizer as? UIPanGestureRecognizer {
+      if let tapRec = otherGestureRecognizer as? UITapGestureRecognizer {
+        return true
+      }
+    }
+    return false
+  }
+  
+  @IBAction func didTap(sender: UITapGestureRecognizer) {
+    swiftris.rotateShape()
   }
   
   func didTick() {
@@ -76,7 +121,10 @@ class GameViewController: UIViewController, SwiftrisDelegate {
   }
   
   func gameShapeDidDrop(swiftris: Swiftris) {
-    
+    scene.stopTicking()
+    scene.redrawShape(swiftris.fallingShape!) {
+      swiftris.letShapeFall()
+    }
   }
   
   func gameShapeDidLand(swiftris: Swiftris) {
